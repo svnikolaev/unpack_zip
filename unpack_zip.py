a72779a58f69d9bbe32460e797ec6c78f6253058
Serg Nikolaev <s.v.nikolaev@sakhalin.gov.ru>
2024-02-29 11:12:08 +1100

    work

--- unpack_zip.py
+++ unpack_zip.py
@@ -4,21 +4,26 @@ import zipfile
 
 
 def unpack_zip(zip_path, encoding='cp866'):
-    # Определение имени архива и директории для распаковки
     zip_path = Path(zip_path)
     unpack_dir = zip_path.parent / zip_path.stem
 
-    # Распаковка архива
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-        # Получаем список файлов в архиве
         file_list = zip_ref.namelist()
-
-        # Если все файлы находятся в одной папке в корне архива
         if len(set(path.split('/')[0] for path in file_list)) == 1:
             unpack_dir = zip_path.parent
 
         for info in zip_ref.infolist():
-            info.filename = info.filename.encode('cp437').decode(encoding)
+            try:
+                # Попытка перекодирования из 'cp437' в целевую кодировку
+                info.filename = info.filename.encode('cp437').decode(encoding)
+            except UnicodeEncodeError:
+                try:
+                    # Если перекодирование не удалось
+                    info.filename = info.filename
+                except UnicodeDecodeError:
+                    print(f"Cannot decode filename: {info.filename}")
+                    continue  # Пропускаем файл с недекодируемым именем
+
             zip_ref.extract(info, unpack_dir)
 
 
