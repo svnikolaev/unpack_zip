c96db6b775e37b9d92034d7d248666a7d6a6a167
Serg Nikolaev <s.v.nikolaev@sakhalin.gov.ru>
2024-02-29 11:19:48 +1100

    better comments remove encodign arg

--- unpack_zip.py
+++ unpack_zip.py
@@ -3,35 +3,38 @@ from pathlib import Path
 import zipfile
 
 
-def unpack_zip(zip_path, encoding='cp866'):
+def unpack_zip(zip_path: str) -> None:
+    """
+    Функция для распаковки zip-архива.
+    :param zip_path: Путь до zip-архива.
+    """
     zip_path = Path(zip_path)
     unpack_dir = zip_path.parent / zip_path.stem
 
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         file_list = zip_ref.namelist()
+        # Если все файлы в одной папке внутри архива, то распаковываем в текущую директорию
         if len(set(path.split('/')[0] for path in file_list)) == 1:
             unpack_dir = zip_path.parent
 
         for info in zip_ref.infolist():
             try:
-                # Попытка перекодирования из 'cp437' в целевую кодировку
-                info.filename = info.filename.encode('cp437').decode(encoding)
+                # Попытка перекодирования из 'cp437' в "cp866"
+                info.filename = info.filename.encode('cp437').decode('cp866')
             except UnicodeEncodeError:
                 try:
-                    # Если перекодирование не удалось
+                    # Если перекодирование не удалось, оставляем исходное имя файла
                     info.filename = info.filename
                 except UnicodeDecodeError:
-                    print(f"Cannot decode filename: {info.filename}")
+                    print(f"Не удалось декодировать имя файла: {info.filename}")
                     continue  # Пропускаем файл с недекодируемым именем
 
-            zip_ref.extract(info, unpack_dir)
+            zip_ref.extract(info, unpack_dir)  # Распаковываем файл
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Unpack a zip archive.')
-    parser.add_argument('zip_path', type=str, help='Path to the zip archive')
-    parser.add_argument('-e', '--encoding', type=str, default='cp866', 
-                        help='Encoding of the file names in the zip archive. Common options are "cp437", "cp866", "cp1251", "utf-8". Default is "cp866".')
+    parser = argparse.ArgumentParser(description='Распаковка zip-архива.')
+    parser.add_argument('zip_path', type=str, help='Путь до zip-архива')
 
     args = parser.parse_args()
-    unpack_zip(args.zip_path, args.encoding)
+    unpack_zip(args.zip_path)
